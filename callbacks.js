ee1c0d078d4b4c81496bf83f152612e256fe4470
Jahzeel Kiel Peras <jhzlklprs@gmail.com>
2023-05-23 00:06:41 +0800

    callbacks update

--- callbacks.js
+++ callbacks.js
@@ -4,24 +4,3 @@ const posts = [
 ];
 
 
-function getPosts() {
-    setTimeout(() => {
-        let output = '';
-        posts.forEach((post, index) => {
-            output += `<li>${post.title}</li>`
-        });
-        document.body.innerHTML = output;
-    }, 1000);
-}
-
-
-
-function createPost(post, callback){
-    setTimeout(() => {
-        posts.push(post);
-        callback();
-    }, 2000);
-}
-
-
-createPost( { title: 'Post Three', body: 'This is post three'}, getPosts);
