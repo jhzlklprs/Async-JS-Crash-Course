23df57bb812b13f9e38fac69ba454d1aab002a5c
Jahzeel Kiel Peras <jhzlklprs@gmail.com>
2023-05-23 00:42:29 +0800

    promises

--- promises.js
+++ promises.js
@@ -1,93 +1,53 @@
 const posts = [
     { title: 'Post One', body: 'This is post one' },
-    { title: 'Post Two', body: 'This is post two' }
+    { title: 'Post Two', body: 'This is post two' },
+    { title: 'Post Three', body: 'This is post three' }
 ];
 
 
-function getPosts() {
+
+
+function getPosts(){
+
     setTimeout(() => {
+
         let output = '';
         posts.forEach((post, index) => {
-            output += `<li>${post.title}</li>`
+            output += `<li>${post.title}</li>`;
         });
+
         document.body.innerHTML = output;
+
     }, 1000);
+
 }
 
-
-
 function createPost(post){
-    return new Promise((resolve, reject) => {
+
+    return new Promise((resolve, reject)=>{
         setTimeout(() => {
             posts.push(post);
 
-            const error = false;
+            const error = true;
 
-            if(!error) {
+            if(!error){
                 resolve();
+
             } else {
-                reject('Error: Something went wrong');
+                reject('Error creating post');
             }
 
-        }, 2000);
-    });
-}
-
-// createPost({ title: 'Post Three', body: 'This is post three' })
-//     .then(getPosts)
-//     .catch(err => console.log(err));
 
 
+        }, 1000);
+
+    })
 
 
-// Async/Await
-// async function init(){
-//     await createPost({ title: 'Post Three', body: 'This is post three' });
-
-//     getPosts();
-// }
-
-
-// init();
-
-
-
-
-// Async / Await / Fetch
-async function fetchUsers(){
-    const res = await fetch
-    ('https://jsonplaceholder.typicode.com/users')
-
-    const data = await res.json();
-
-    console.table(data);
 }
 
 
-fetchUsers();
+createPost({ title: 'Post Four', body: 'This is post four' })
+.then(getPosts)
+.catch(err => console.error(err));
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//Promise.all
-// const promise1 = Promise.resolve('Hello World');
-// const promise2 = 10;
-// const promise3 = new Promise((resolve, reject) => 
-//     setTimeout(resolve, 2000, 'Goodbye'));
-
-
-// const promise4 = fetch('https://jsonplaceholder.typicode.com/users').then(res => res.json());
-
-// Promise.all([promise1,promise2,promise3]).then((values) => console.log(values));
